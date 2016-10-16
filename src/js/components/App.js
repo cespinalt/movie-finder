aae7a6fd5fe09ad3b60c608be2dbb8a1c9a9c920
Carlos Espinal <cespinalt@gmail.com>
2016-10-15 18:53:07 -0700

    Include SearchForm component

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -2,13 +2,47 @@ const React = require('react');
 const Component = require('react').Component;
 const AppActions = require('../actions/AppActions');
 const AppStores = require('../stores/AppStores');
+const SearchForm = require('./SearchForm.jsx');
+
+function getAppState() {
+  return {
+    movies: AppStores.getMovieResults()
+  }
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+      AppStores.addChangeListener(this._onChange.bind(this));
+  }
+
+  componentWillUnmount() {
+    AppStores.removeChangeListener(this._onChange.bind(this));
+  }
+
+  _onChange() {
+    this.setState(getAppState());
+  }
+
   render() {
+    console.log(this.state.movies);
     return(
-      <div>My App</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-8 col-xs-offset-2">
+            <div className="well">
+              <SearchForm />
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
+
 }
 
 module.exports = App;

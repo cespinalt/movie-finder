1fe3acb07977868a04be3fc94f596b6dbc9332c2
Carlos Espinal <cespinalt@gmail.com>
2016-10-15 17:01:11 -0700

    Add boilerplate code for flux

---
+++ src/js/components/App.js
@@ -0,0 +1,14 @@
+const React = require('react');
+const Component = require('react').Component;
+const AppActions = require('../actions/AppActions');
+const AppStores = require('../stores/AppStores');
+
+class App extends Component {
+  render() {
+    return(
+      <div>My App</div>
+    )
+  }
+}
+
+module.exports = App;

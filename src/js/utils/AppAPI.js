9d7c766bcc28d8b1b3bf2aeaae2af1398b394136
Carlos Espinal <cespinalt@gmail.com>
2016-10-15 18:57:45 -0700

    Build get request to get the movies

--- src/js/utils/AppAPI.js
+++ src/js/utils/AppAPI.js
@@ -1,7 +1,20 @@
 const AppActions = require('../actions/AppActions');
+const axios = require('axios');
 
 module.exports = {
   searchMovies: (movie) => {
-
+    axios.get('http://www.omdbapi.com/', {
+      params: {
+        s: movie.title,
+      }
+    })
+    .then(res => {
+      if(!res.data.Error) {
+        AppActions.receiveMovieResults(res.data.Search);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
   }
 };

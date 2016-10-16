d47a5fed3fe17bbfee7011ede4fc1dc4d2c87d73
Carlos Espinal <cespinalt@gmail.com>
2016-10-15 18:49:24 -0700

    Add searchMovies and receiveMovieResults methods
    ReceiveMovieResults passes RECEIVE_MOVIE_RESULTS constant and an
    array of movies

--- src/js/actions/AppActions.js
+++ src/js/actions/AppActions.js
@@ -2,7 +2,18 @@ const AppDispatcher = require('../dispatcher/AppDispatcher');
 const AppConstants = require('../constants/AppConstants');
 
 const AppActions = {
-
+  searchMovies: movie => {
+    AppDispatcher.handleViewAction({
+      actionType: AppConstants.SEARCH_MOVIES,
+      movie,
+    });
+  },
+  receiveMovieResults: movies => {
+    AppDispatcher.handleViewAction({
+      actionType: AppConstants.RECEIVE_MOVIES_RESULTS,
+      movies,
+    })
+  }
 };
 
 module.exports = AppActions;
